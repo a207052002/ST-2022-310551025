ac9bde11e436b1ff1fc3ab975e31bbcbf0c69c43
a207052002 <a207052002@gmail.com>
2022-03-17 08:13:24 +0800

    change code back to pass tests

--- src/test/java/PriorityQueueTest.java
+++ src/test/java/PriorityQueueTest.java
@@ -15,7 +15,7 @@ class dummyClass { }
 public class PriorityQueueTest {
     static Stream<Arguments> streamProvider() {
         return Stream.of(
-                Arguments.of(new int[]{11, 2, 1, 99}, new int[]{0, 1, 2, 11, 99}),
+                Arguments.of(new int[]{11, 2, 1, 99, 0}, new int[]{0, 1, 2, 11, 99}),
                 Arguments.of(new int[]{5, 4, 2, 3}, new int[]{2, 3, 4, 5}),
                 Arguments.of(new int[]{1, 2, 1, -3, 66}, new int[]{-3, 1, 1, 2, 66}),
                 Arguments.of(new int[]{0, 1, 1, 99, -99}, new int[]{-99, 0, 1, 1, 99}),
